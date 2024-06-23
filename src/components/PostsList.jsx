47ddbb384b11f0ab717b6d712295dc1ea796ce92
Caio Villa Silva Tsubake <caiotsubake@gmail.com>
2024-06-23 18:24:12 -0300

    Adding NewPost component to the page

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,12 +1,16 @@
 import Post from "./Post";
-import classes from "./PostsList.module.css"
+import classes from "./PostsList.module.css";
+import NewPost from "./NewPost";
 
 function PostsList() {
   return (
-    <ul className={classes.posts}>
-      <Post author="Maximillian" body="React.js is awesome!" />
-      <Post author="Manuel" body="Check-out the full course!" />
-    </ul>
+    <>
+      <NewPost></NewPost>
+      <ul className={classes.posts}>
+        <Post author="Maximillian" body="React.js is awesome!" />
+        <Post author="Manuel" body="Check-out the full course!" />
+      </ul>
+    </>
   );
 }
 
