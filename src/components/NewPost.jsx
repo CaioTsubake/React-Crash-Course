e19c13d8066798ad8482968a9aa5ea6dc87c3c1e
Caio Villa Silva Tsubake <caiotsubake@gmail.com>
2024-07-14 17:35:14 -0300

    Lifting state to a new component
    This allow components to respond to  events that happened in another component

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import classes from './NewPost.module.css';
 
-function NewPost() {
+function NewPost(props) {
   const [ enteredBody, setEnteredBody ] = useState(''); 
 
   function changeBodyHandler(event){
@@ -12,12 +12,11 @@ function NewPost() {
     <form className={classes.form}>
       <p>
         <label htmlFor="body">Text</label>
-        <textarea id="body" required rows={3} onChange={changeBodyHandler} />
+        <textarea id="body" required rows={3} onChange={props.onBodyChange} />
       </p>
-      <p>{enteredBody}</p>
       <p>
         <label htmlFor="name">Your name</label>
-        <input type="text" id="name" required />
+        <input type="text" id="name" required onChange={props.onAuthorChange}/>
       </p>
     </form>
   );
