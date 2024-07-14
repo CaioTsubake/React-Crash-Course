0a5f5d75af7bb5e10bc326c5b35567e436555e9b
Caio Villa Silva Tsubake <caiotsubake@gmail.com>
2024-07-14 17:46:32 -0300

    Using  the'children' property
    This is used to reference the content that is wrapped by the modal

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,23 +1,29 @@
 import Post from "./Post";
 import classes from "./PostsList.module.css";
 import NewPost from "./NewPost";
+import Modal from "./Modal";
 import { useState } from "react";
 
 function PostsList() {
-  const [enteredBody, setEnteredBody] = useState('');
-  const [enteredAuthor, setEnteredAuthor] = useState('')
+  const [enteredBody, setEnteredBody] = useState("");
+  const [enteredAuthor, setEnteredAuthor] = useState("");
 
-  function bodyChangeHandler(event){
+  function bodyChangeHandler(event) {
     setEnteredBody(event.target.value);
   }
 
-  function authorChangeHandler(event){
+  function authorChangeHandler(event) {
     setEnteredAuthor(event.target.value);
   }
 
   return (
     <>
-      <NewPost onBodyChange={bodyChangeHandler} onAuthorChange={authorChangeHandler}></NewPost>
+      <Modal>
+        <NewPost
+          onBodyChange={bodyChangeHandler}
+          onAuthorChange={authorChangeHandler}
+        ></NewPost>
+      </Modal>
       <ul className={classes.posts}>
         <Post author={enteredAuthor} body={enteredBody} />
         <Post author="Manuel" body="Check-out the full course!" />
